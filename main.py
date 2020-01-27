c6872f693b9273de85ed4c09b0ac7b213e4fc253
Piyapong <u4507075@hotmail.com>
2020-01-27 11:18:53 +0700

    clinical note map demo

---
+++ main.py
@@ -0,0 +1,32 @@
+#import libraries
+import numpy as np
+import pandas as pd
+import re
+from preprocess.compare_text import compare
+
+from nltk.corpus import stopwords
+
+# You will have to download the set of stop words the first time
+#import nltk
+#nltk.download('stopwords')
+stop_words = stopwords.words('english')
+
+#secret directory to the dataset
+path = '../secret/data/'
+
+
+#start your code here
+discharge_summary = pd.read_csv(path+'discharge_summary.csv',index_col=0)
+discharge_summary = discharge_summary.head(10)
+
+discharge_summary['sum_note'] = discharge_summary['sum_note'].apply(lambda x: re.sub('&lt;br/&gt;',' ',str(x).lower()))
+discharge_summary['sum_note'] = discharge_summary['sum_note'].apply(lambda x: re.sub('&gt;',' ',str(x).lower()))
+discharge_summary['sum_note'] = discharge_summary['sum_note'].apply(lambda x: re.sub('<.*?>',' ',str(x).lower()))
+discharge_summary['sum_note'] = discharge_summary['sum_note'].apply(lambda x: re.sub('[^a-zA-Z0-9 ]','',str(x).lower()))
+discharge_summary['sum_note'] = discharge_summary['sum_note'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
+discharge_summary['sum_note'] = discharge_summary['sum_note'].apply(lambda x: re.sub('  +',' ',str(x).lower()))
+
+for index,row in discharge_summary.head(10).iterrows():
+	print(row['sum_note'])
+	compared_data = pd.read_csv(path+'snomed/finding.csv',index_col=0)
+	compare(row['sum_note'],compared_data.copy())
---
+++ preprocess/compare_text.py
@@ -0,0 +1,14 @@
+from fuzzywuzzy import fuzz
+import pandas as pd
+import re
+
+def text_similarity(x,y):
+  return fuzz.token_set_ratio(x.lower(),y.lower())
+
+def compare(text,compared_data):
+	compared_data['similarity'] = compared_data['term'].apply(lambda x: text_similarity(str(text),str(x)))
+	compared_data = compared_data.drop_duplicates()
+	compared_data = compared_data.sort_values(by='similarity', ascending=False)
+	compared_data = compared_data[compared_data['similarity']>80]
+	print(compared_data)
+
